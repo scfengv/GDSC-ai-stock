10cb3336568a83431fd00f9d065f733e2a702b06
DESKTOP-COR4OEI\陳子雋 <timchen975@gmail.com>
2024-03-01 23:36:07 +0800

    modify some comments

--- pre_trained.py
+++ pre_trained.py
@@ -22,17 +22,16 @@ for filename in tqdm(os.listdir(data_folder), desc="Total file"):
         for tweet_id, tweet_info in tqdm(data.items(), desc="Current file processing"):
             tweet_text = tweet_info["text"]
             output = query({"inputs": tweet_text})
-            # 創建情感分析結果的字典
+            # 創建情感分析結果的字典(後續加入原JSON檔)
             sentiment_score = {}
             for item in output[0]:
                 label = item["label"]
                 score = item["score"]
                 sentiment_score[label] = score
 
-            # 將情感分析結果加入到推文資料中
             tweet_info["sentiment_score"] = sentiment_score
 
-        # 將更新後的 JSON 資料寫回原始 JSON 檔案
+        # 更新JSON檔案
         with open(file_path, "w") as file:
             json.dump(data, file, indent=4)
 print("Finish!")
