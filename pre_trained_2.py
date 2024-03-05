e68c1aee273cb80e87858324b9c29e2d3837f592
DESKTOP-RIVTRKI\user <timchen975@gmail.com>
2024-03-05 16:59:57 +0800

    modify the python file

--- pre_trained_2.py
+++ pre_trained_2.py
@@ -36,23 +36,27 @@ for filename in tqdm(os.listdir(data_folder), desc="Total file"):
             json.dump(data, file, indent=4)
 
 # 改標籤名稱
-for filename in tqdm(os.listdir(data_folder), desc="Total file"):
+for filename in tqdm(os.listdir(data_folder), desc="Modify the label name"):
     if filename.endswith(".json"):
         file_path = os.path.join(data_folder, filename)
         with open(file_path, "r") as file:
             data = json.load(file)
+
         for tweet_id, tweet_info in data.items():
-            if "LABEL_1" in tweet_info["sentiment_score"]:
-                tweet_info["sentiment_score"]["neutral"] = tweet_info[
-                    "sentiment_score"
-                ].pop("LABEL_1")
-            if "LABEL_2" in tweet_info["sentiment_score"]:
-                tweet_info["sentiment_score"]["positive"] = tweet_info[
-                    "sentiment_score"
-                ].pop("LABEL_2")
-            if "LABEL_0" in tweet_info["sentiment_score"]:
-                tweet_info["sentiment_score"]["negative"] = tweet_info[
-                    "sentiment_score"
-                ].pop("LABEL_0")
+            sentiment_score = tweet_info.get("sentiment_score", {})
+            # 對情感分析字典進行修改
+            if "LABEL_1" in sentiment_score:
+                sentiment_score["neutral"] = sentiment_score.pop("LABEL_1")
+            if "LABEL_2" in sentiment_score:
+                sentiment_score["positive"] = sentiment_score.pop("LABEL_2")
+            if "LABEL_0" in sentiment_score:
+                sentiment_score["negative"] = sentiment_score.pop("LABEL_0")
+            # 將修改後的情感分析字典重新賦值給tweet_info
+            tweet_info["sentiment_score"] = sentiment_score
+
+        # 寫回JSON檔案
+        with open(file_path, "w") as file:
+            json.dump(data, file, indent=4)
+
 
 print("Finish!")
