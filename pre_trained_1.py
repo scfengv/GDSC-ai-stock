5016e0e192168384b6d636d18e80e07c9ac7f3b1
DESKTOP-RIVTRKI\user <timchen975@gmail.com>
2024-03-11 17:53:28 +0800

    Add the weighted.py and update the dataset

--- pre_trained_1.py
+++ pre_trained_1.py
@@ -6,7 +6,7 @@ import requests
 from tqdm import tqdm
 
 API_URL = "https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-sentiment-latest"
-headers = {"Authorization": "Bearer APIKEY"}
+headers = {"Authorization": "Bearer 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"}
 
 
 def query(payload):
---
+++ weighted.py
@@ -0,0 +1,27 @@
+import os
+import json
+
+data_folder_list = ["Data_1", "Data_2"]
+for data_folder in data_folder_list:
+    for filename in os.listdir(data_folder):
+        if filename.endswith(".json"):
+            file_path = os.path.join(data_folder, filename)
+            with open(file_path, "r") as file:
+                data = json.load(file)
+                weights = {"likes": 0.1, "replies": 0.3, "retweets": 0.6}
+                for tweet_info in data.values():
+                    weighted_sentiment_score = {}
+                    weighted_sum = (
+                        tweet_info["normalized_likes"] * weights["likes"]
+                        + tweet_info["normalized_replies"] * weights["replies"]
+                        + tweet_info["normalized_retweets"] * weights["retweets"]
+                    )
+                    print(weighted_sum)
+                    for label, score in tweet_info["sentiment_score"].items():
+                        weighted_sentiment_score[label] = (
+                            tweet_info["sentiment_score"][label] * weighted_sum
+                        )
+                    tweet_info["weighted_sentiment_score"] = weighted_sentiment_score
+
+                with open(file_path, "w") as file:
+                    json.dump(data, file, indent=4)
